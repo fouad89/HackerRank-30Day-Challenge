e945deafccd2ebd5a13b6d25ecc47981157d8ca5
Fouad <fouad89ahmed@gmail.com>
2021-08-01 22:34:18 +0100

    starting

---
+++ day11-2DArrays.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* Given a 6 x 6 array of 1s and 0s, find 
+Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+an hour glass has the format of:
+a b c
+  d
+e f g
+
+ */
+
+int main(){
+    int rowSize = 6;
+    int colSize = 6;
+    int myArr [rowSize][colSize];
+    int testArr [6][6]= {
+        {0, 1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10, 11},
+        {12, 13, 14, 15, 16, 17},
+        {18, 19, 20, 21, 22, 23},
+        {24, 25, 26, 27, 28, 29},
+        {30, 31, 32, 33, 34,35}
+    };
+
+    for (int i = 0; i < rowSize; i++)
+
+
+
+    return 0;
+}

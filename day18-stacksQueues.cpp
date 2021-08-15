d45e91e6545a74f0774815ef7e46c37be32ca275
Fouad <fouad89ahmed@gmail.com>
2021-08-15 09:16:20 +0100

    day 18 queues and stacks

---
+++ day18-stacksQueues.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+/* 
+defenition: 
+    A stack pushes a new element to the top of the stack and also pops the element at the top. 
+    A queue, however, dequeues (removes) an element from the top of the queue, ​but it enqueues (inserts) an element at the bottom.
+
+Write the following declarations and implementations:
+
+Two instance variables: one for your stack, and one for your queue.
+A void pushCharacter(char ch) method that pushes a character onto a stack.
+A void enqueueCharacter(char ch) method that enqueues a character in the queue instance variable.
+A char popCharacter() method that pops and returns the character at the top of the stack instance variable.
+A char dequeueCharacter() method that dequeues and returns the first character in the queue instance variable.
+
+ */
